28986eefcf97261744f157f52c6c7a38514df5f2
Kruemmelbande <kai.scheufen@gmail.com>
2022-10-26 09:17:17 +0200

    it checks for python version at runtime now

--- stoopidScript.py
+++ stoopidScript.py
@@ -2,6 +2,13 @@ import array
 from dataclasses import replace
 import locale
 from sys import argv as args, exit
+import platform
+
+plt=platform.python_version().split(".")
+if plt[0]=="3" and plt[1]>"10":
+    print("python >3.10 is required")
+if plt[0]<"3":
+    print("python >3.10 is required")
 vars={}
 file="helloWorld.stsc"
 file = args[1]
